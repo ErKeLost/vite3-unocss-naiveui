205977ec593d9a6601d3b58beade769d8c591fbd
ERKELSOT <1256029807@qq.com>
2022-09-27 22:18:58 +0800

    chore: paopao

--- src/views/login/cpns/loginForm.vue
+++ src/views/login/cpns/loginForm.vue
@@ -25,7 +25,13 @@ async function handleLogin() {
 <template>
   <div w-full>
     <div flex justify-center>
-      <Twitter w-15 h-15 />
+      <div class="box">
+        <div class="paopao"></div>
+        <div class="shadow"></div>
+      </div>
+    </div>
+    <div flex justify-center>
+      <!-- <Twitter w-15 h-15 /> -->
     </div>
     <div pt-5>
       <span block my-3>Username</span>
@@ -54,5 +60,63 @@ async function handleLogin() {
 <style scoped>
 .button {
   letter-spacing: 2px;
+}
+
+.box {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 100px;
+  position: relative;
+}
+.paopao {
+  width: 100%;
+  height: 100%;
+  background: radial-gradient(
+    circle at 70% 30%,
+    #c4cb78 10%,
+    #adbf94 60%,
+    #ca815a 100%
+  );
+  border-radius: 50%;
+  box-shadow: inset 0 0 20px #fff, inset 10px 0 50px #eaf5fc,
+    inset 80px 0 60px #efcde6, inset -20px -60px 100px #f9f6de,
+    inset 0 50px 150px #f9f6de, 0 0 90px #fff;
+  animation: move 3s ease-in-out infinite;
+}
+.shadow {
+  position: absolute;
+  left: 70px;
+  bottom: -100px;
+  width: 150px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: #282826;
+  filter: blur(5px);
+  animation: move1 3s ease-in-out infinite;
+}
+@keyframes move {
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-90px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+@keyframes move1 {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(0.5);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
