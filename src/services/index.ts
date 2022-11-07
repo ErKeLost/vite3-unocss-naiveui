0f34b56c9e94065581a6bc62dd0a4b8d42c1cc4c
erkelost <1256029807@qq.com>
2022-11-07 17:16:00 +0800

    chore: test

--- src/services/index.ts
+++ src/services/index.ts
@@ -9,7 +9,8 @@ const serviceGlobalFiles = getGlobalFileExport(serviceModules)
 // const { VITE_SERVICE_TIME_OUT, VITE_GLOB_API_URL_PREFIX } = import.meta.env
 const request = new Request({
   // baseURL: VITE_GLOB_API_URL_PREFIX,
-  baseURL: 'https://chat-erkelost.vercel.app',
+  baseURL: '/api',
+  // https://chat-erkelost.vercel.app
   // baseURL: 'http://47.95.215.156:3333',
   // timeout: VITE_SERVICE_TIME_OUT,
   timeout: 60000,
